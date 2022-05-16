57aa64d807d230eb9ba9e34334c655d6fcc391fb
Guilherme Marques Mesquita <g.m.mesquita@hotmail.com>
2022-05-15 22:54:51 -0300

    feat: new features

--- script.js
+++ script.js
@@ -9,11 +9,9 @@ function Chutar() {
   } else if (chute > 10 || chute < 0) {
     elementoResultado.innerHTML = "Digite um número de 0 a 10";
   } else {
-    elementoResultado.innerHTML = "Errooooou! O número era " + numeroSecreto;
+    elementoResultado.innerHTML = "Que pena, você errou.";
   }
   if (chute > 10 || chute < 0) {
     alert("Digite um número de 0 a 10");
-  } else {
-    alert("Tente novamente!");
   }
 }
